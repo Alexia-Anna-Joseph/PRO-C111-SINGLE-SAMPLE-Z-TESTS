8b000ae4d8669cfb16ee81e5c5784235c11d5e2e
Alexia-Anna-Joseph <78249362+Alexia-Anna-Joseph@users.noreply.github.com>
2022-04-10 10:53:57 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -25,12 +25,12 @@ def random_set_of_mean(counter):
     mean = statistics.mean(dataset)
     return mean
 
-def setup():
-    mean_list = []
-    for i in range(0,1000):
-        set_of_means= random_set_of_mean(30)
-        mean_list.append(set_of_means)
-    show_fig(mean_list)
+
+mean_list = []
+for i in range(0,1000):
+    set_of_means= random_set_of_mean(30)
+    mean_list.append(set_of_means)
+    
 
 def show_fig(mean_list):
     df=mean_list
@@ -56,7 +56,7 @@ fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end], y=
 fig.show()
 
 #finding the mean of the first data(STUDENTS WHO GOT TABLET WITH LEARNING MATERIAL) and plotting it on the plot.
-df = pd.read_csv("data1.csv")
+df = pd.read_csv("sample_1.csv")
 data = df["Math_score"].tolist()
 mean_of_sample1 = statistics.mean(data)
 print("Mean of sample1:- ",mean_of_sample1)
@@ -66,7 +66,7 @@ fig.add_trace(go.Scatter(x=[mean_of_sample1, mean_of_sample1], y=[0, 0.17], mode
 fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
 fig.show()
 
-df = pd.read_csv("data2.csv")
+df = pd.read_csv("sample_2.csv")
 data = df["Math_score"].tolist()
 mean_of_sample2 = statistics.mean(data)
 print("Mean of sample2:- ",mean_of_sample2)
@@ -76,7 +76,7 @@ fig.add_trace(go.Scatter(x=[mean_of_sample2, mean_of_sample2], y=[0, 0.17], mode
 fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
 fig.show()
 
-df = pd.read_csv("data3.csv")
+df = pd.read_csv("sample_3.csv")
 data = df["Math_score"].tolist()
 mean_of_sample3 = statistics.mean(data)
 print("Mean of sample3:- ",mean_of_sample3)
